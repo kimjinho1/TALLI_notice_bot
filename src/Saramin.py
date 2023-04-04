5f677e273ac6c17f29618b662c29a5d29d8b47b9
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-05 06:42:47 +0900

    FEAT: 모든 키워드 일단 변수로 저장 완료

--- src/Saramin.py
+++ src/Saramin.py
@@ -22,16 +22,21 @@ TODO: 사람인 크롤링
 - 스크랩 수
 - 공고명
 
-- 우대사항
-
-TODO:
 - 경력
 - 학력
 - 근무형태
 - 급여
 - 근무일시
+
+아주 드믈게 없는 경우 있음
 - 근무지역
+
+아래 2개는 없는 경우도 있음
+- 필수사항
+- 우대사항
+
 - 접수 시작일
+마감일은 없는 경우도 있음 -> 채용시 마감
 - 접수 마감일
 """
 
@@ -49,49 +54,99 @@ class Saramin:
         self.data = {}
 
     def crawling(self):
-        page = 1
-        while True:
-            search_url = f"{self.base_url}&searchword={self.search_words[0]}&recruitPage={page}"
-            search_response = requests.get(search_url, headers=self.headers[0])
-            search_soup = BeautifulSoup(search_response.content, "lxml")
+        for search_word in self.search_words:
+            page = 1
+            search_url = f"{self.base_url}&searchword={search_word}&recruitPage={page}"
 
-            # 마지막 페이지 확인
-            finish = search_soup.select_one("div.info_no_result") 
-            if finish is not None:
+            while True:
+                search_response = requests.get(search_url, headers=self.headers[0])
+                search_soup = BeautifulSoup(search_response.content, "lxml")
+
+                # 마지막 페이지 확인
+                finish = search_soup.select_one("div.info_no_result") 
+                if finish is not None:
+                    break
+
+                # 채용 공고 링크 리스트 추출[40개]
+                job_tit_list = search_soup.select("h2.job_tit a[href]") 
+                job_tit_links = []
+                for job_tit in job_tit_list:
+                    job_tit_link = self.origin_url + job_tit["href"]
+                    job_tit_links.append(job_tit_link)
+
+                # 채용 공고 페이지로 이동
+                for link in job_tit_links:
+                    # url
+                    url = link.replace("relay/", "")
+                    response = requests.get(url, headers=self.headers[0])
+                    soup = BeautifulSoup(response.content, "lxml")
+
+                    # 회사, 공고명, 스크랩 수
+                    company = soup.select_one("div.title_inner a.company").text.strip().replace('\\xa0', ' ')
+                    title = soup.select_one("h1.tit_job").text.strip().replace('\\xa0', ' ')
+                    scrap_count = soup.select_one("div.jv_header span.txt_scrap").text.strip().replace('\\xa0', ' ')
+
+                    # 경력, 학력, 근무형태, 급여, 근무일시, 근무지역 추출
+                    # 필수사항, 우대사항, 직급/직책은 없는 경우 있긴 하지만 일단 추출함
+                    # default: dict_keys(['경력', '학력', '근무형태', '급여', '근무일시', '근무지역'])
+                    summary_dict = {}
+                    summary_list = soup.select("div.jv_summary div.col dl")
+                    for li in summary_list:
+                        key = li.select_one("dt").text.strip().replace('\\xa0', ' ')
+                        dd_tag = li.select_one("dd")
+                        dd_first_str = str(next(dd_tag.stripped_strings)).strip().replace('\\xa0', ' ')
+                        details = dd_tag.select("ul.toolTipTxt li")
+                        details_str = ""
+                        # 상세보기가 있는 경우
+                        if details:
+                            for detail in details:
+                                detail_key = detail.select_one("span").text.strip().replace('\\xa0', ' ')
+                                detail_key = "".join(detail_key.split())
+                                detail_val = detail.text.strip().replace('\\xa0', ' ')[len(detail_key):]
+                                if details_str == "":
+                                    details_str += f"{detail_key}:{detail_val}"
+                                else:
+                                    details_str += f"|{detail_key}:{detail_val}"
+                        # 기본 텍스트가 없고, 상세보기만 있는 경우
+                        if key == dd_first_str:
+                            val = details_str
+                        else:
+                            val = dd_tag.text.strip().replace('\\xa0', ' ')
+                            # 기본 텍스트, 상세보기 둘 다 있는 경우
+                            if details:
+                                details_origin_str = li.select_one("dd").select_one("div.toolTipWrap").text
+                                val = val[:-len(details_origin_str) + len(details) + 1]
+                        summary_dict[key] = val
+
+                    # 근무지역이 뒤에 지도 텍스트가 같이 들어와서 자름
+                    if "근무지역" in summary_dict:
+                        summary_dict["근무지역"] = " ".join(summary_dict["근무지역"].split()[:-1])
+
+
+                    period_list = soup.select("dl.info_period dd") 
+                    # YYYY.MM.DD HH:MM 형식
+                    start = period_list[0].text.strip().replace('\\xa0', ' ')
+                    # 마감일은 없는 경우 있음
+                    if len(period_list) == 2:
+                        end = period_list[1].text.strip().replace('\\xa0', ' ')
+                    else:
+                        end = "채용시 마감"
+
+                    print("search_word:", search_word)
+                    print("page:", page)
+                    print("url:", url)
+                    print("company:", company)
+                    print("title:", title)
+                    print("scrap_count:", scrap_count)
+                    for k, v in summary_dict.items():
+                        print(f"{k}: {v}")
+                    print("start:", start)
+                    print("end:", end)
+                    print()
+                    break
+                    
+                page += 1
                 break
-
-            # 채용 공고 링크 리스트 추출[40개]
-            job_tit_list = search_soup.select("h2.job_tit a[href]") 
-            job_tit_links = []
-            for job_tit in job_tit_list:
-                job_tit_link = self.origin_url + job_tit["href"]
-                job_tit_links.append(job_tit_link)
-
-            for link in job_tit_links:
-                # url
-                url = link.replace("relay/", "")
-                response = requests.get(url, headers=self.headers[0])
-                soup = BeautifulSoup(response.content, "lxml")
-
-                # 회사, 공고명, 스크랩 수
-                company = soup.select_one("div.title_inner a.company").text.strip()
-                title = soup.select_one("h1.tit_job").text.strip()
-                scrap_count = soup.select_one("div.jv_header span.txt_scrap").text.strip()
-
-                # 우대사항
-                preferred_dict = {}
-                preferred_list = soup.select("dd.preferred ul.toolTipTxt li")
-                for li in preferred_list:
-                    key = li.select_one("span").text.strip()
-                    val = li.text[len(key):].strip()
-                    preferred_dict[key] = val
-
-                                   
-
-                break
-
-            page += 1
-            break
 
 
     def save_result(self, keyword):
