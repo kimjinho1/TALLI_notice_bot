752eb3dee5ccc1bf190e9f0a9957fbaddbb8c1b3
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-29 05:12:53 +0900

    chore: 필요 없는 함수 삭제

--- src/utils.py
+++ src/utils.py
@@ -26,21 +26,6 @@ def text_filter(text):
         text = text.replace("  ", " ")
     return text
 
-def print_data(data):
-    print("url:", data["url"])
-    print("기업명:", data["기업명"])
-    print("공고명:", data["공고명"])
-    print("스크랩 수:", data["스크랩 수"])
-    print("경력:", data["경력"])
-    print("학력:", data["학력"])
-    print("근무형태:", data["근무형태"])
-    print("급여:", data["급여"])
-    print("근무지역:", data["근무지역"])
-    print("필수사항:", data["필수사항"])
-    print("우대사항:", data["우대사항"])
-    print("접수 시작일:", data["접수 시작일"])
-    print("접수 마감일:", data["접수 마감일"])
-
 if __name__ == "__main__":
     print(get_yesterday_date())
     print(get_current_date())
