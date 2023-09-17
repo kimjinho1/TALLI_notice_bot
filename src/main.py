17b9644801f891b6bc783b1a2cd8d23c61793c8a
kimjinho1 <rlawlsgh8113@naver.com>
2023-09-18 01:46:03 +0900

    chore: 주석 수정

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
     saramin = Saramin(day="today", save=False)
     saramin.crawling()
     file_path = saramin.save_all_data_to_csv()
+
     slack_bot = SlackBot()
-    # 채용 공고 존재함 -> 파일 전송
     if (file_path != ""): 
+        # 채용 공고 존재함 -> 파일 전송
         slack_bot.send_file(file_path)
-    # 채용 공고 없음 -> 메시지 전송
     else:
+        # 채용 공고 없음 -> 메시지 전송
         slack_bot.send_message("오늘은 등록된 공고가 없습니다!")
