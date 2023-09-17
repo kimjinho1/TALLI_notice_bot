08322fcec8895ced8960a667af9caf3046608db0
kimjinho1 <rlawlsgh8113@naver.com>
2023-09-18 04:31:18 +0900

    feat: 근무지 위치 파싱 로직 추가

--- src/Saramin.py
+++ src/Saramin.py
@@ -25,6 +25,7 @@ class Saramin:
             page = 1
             idx = 0
             result = pd.DataFrame()
+            
             while True:
                 search_url = f"{self.base_url}&searchword={search_word}&recruitPage={page}&except_read=&ai_head_hunting=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7"
                 search_response = requests.get(
@@ -109,12 +110,63 @@ class Saramin:
                                     "dd").select_one("div.toolTipWrap").text)
                                 val = val[:-len(details_origin_str) +
                                           len(details) + 1]
+
                         summary_dict[key] = val
 
                     # 근무지역이 뒤에 지도 텍스트가 같이 들어와서 자름
                     if "근무지역" in summary_dict:
-                        summary_dict["근무지역"] = " ".join(
-                            summary_dict["근무지역"].split()[:-1])
+                        job_location_split_li = summary_dict["근무지역"].split()
+                        if job_location_split_li[-1] == "지도":
+                            job_location = " ".join(job_location_split_li[:-1])
+                            summary_dict["근무지역"] = job_location_filter(job_location)
+
+                    # 더 자세한 위치 정보가 담긴 근무지 위치가 있을 시 근무지역 수정 
+                    bottom_job_location_span = soup.select_one("div.jv_cont.jv_location span.spr_jview.txt_adr")
+                    middle_job_location_span = soup.select_one("div.wrap_list_template span#template_job_type_aw_post_address")
+                    extra_job_location_td_list = soup.select("div.wrap_break_recruit td")
+
+                    # Case 1: 하단 근무지 위치가 존재하는 경우
+                    if bottom_job_location_span:
+                        job_location = job_location_filter(bottom_job_location_span.text)
+                        summary_dict["근무지역"] = job_location
+                        # print(f"Case 1: {job_location}")
+                    # Case 2: 중단 근무지 위치가 존재하는 경우
+                    elif middle_job_location_span:
+                        job_location = job_location_filter(middle_job_location_span.text)
+                        summary_dict["근무지역"] = job_location.split(' - ')[0]
+                        # print(f"Case 2: {summary_dict['근무지역']}")
+                    # Case 3: 근무지역이 어딘가에 무조건 들어있긴 해서 텍스트를 다 확인하면 찾을 수는 있다...
+                    elif extra_job_location_td_list:
+                        for td in extra_job_location_td_list:
+                            td_text = td.text
+                            # EX) ㆍ근무지역 : (110-850) 서울 종로구 효제동 20 우일빌딩
+                            # https://www.saramin.co.kr/zf_user/jobs/view?view_type=search&rec_idx=46512518&location=ts&searchword=CRA&searchType=search&paid_fl=n&search_uuid=9c36f5e9-3b77-40fe-949a-afa4a6ea67b5
+                            if "근무지역" in td_text:
+                                # ㆍ근무형태 : 정규직(수습기간 3개월) 
+                                start_idx = td_text.find("근무지역")
+                                end_idx = td_text.find('ㆍ', start_idx + 15)
+                                if end_idx == -1:
+                                    job_location = job_location_filter(td_text[start_idx:])
+                                else:
+                                    job_location = job_location_filter(td_text[start_idx:end_idx])
+                                summary_dict["근무지역"] = job_location_filter(job_location[7:])
+                                # print(f"case 3-1: {summary_dict['근무지역']}")
+                                break
+                            # EX) - 회사주소 : 경기 용인시 수지구 손곡로 17
+                            # https://www.saramin.co.kr/zf_user/jobs/view?view_type=search&rec_idx=46587286&location=ts&searchword=%EB%A9%94%EB%94%94%EC%BB%AC%EB%9D%BC%EC%9D%B4%ED%84%B0&searchType=search&paid_fl=n&search_uuid=060232a0-7395-47f2-9bd7-12a3ef73425f
+                            elif "회사주소" in td_text:
+                                start_idx = td_text.find("회사주소")
+                                end_idx = td_text.find('-', start_idx + 15)
+                                if end_idx == -1:
+                                    job_location = job_location_filter(td_text[start_idx:])
+                                else:
+                                    job_location = job_location_filter(td_text[start_idx:end_idx])
+                                summary_dict["근무지역"] = job_location_filter(job_location[7:])
+                                # print(f"case 3-2: {summary_dict['근무지역']}")
+                                break
+                    # 근무지역 정보가 아예 존재하지 않는 경우
+                    else:
+                        print(f"case 4: 자세한 근무지역 정보가 존재하지 않습니다")
 
                     period_list = soup.select("dl.info_period dd")
                     # YYYY.MM.DD HH:MM 형식
@@ -173,6 +225,7 @@ class Saramin:
                         break
                 if page >= 3 or idx >= 10:
                     break
+                break ##################################################################
                 page += 1
 
             if self.save == True:
