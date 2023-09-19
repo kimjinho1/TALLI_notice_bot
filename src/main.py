b75de19a82610bc79bb462ee9e55e976dcfa40f6
kimjinho1 <rlawlsgh8113@naver.com>
2023-09-19 22:46:43 +0900

    fix: 로직 중간에 에러 발생시 에러 메시지 전송하도록 수정

--- src/main.py
+++ src/main.py
@@ -3,25 +3,34 @@ from SlackBot import SlackBot
 from bigquery import *
 
 if __name__ == "__main__":
-    # day(today: 오늘만, all: 전부)
-    # save(true: csv 저장 O, false: csv 저장 X)
-    # saramin = Saramin(day="all", save=True)
-    saramin = Saramin(day="today", save=False)
-    saramin.crawling()
-    file_path = saramin.save_all_data_to_csv()
-
     slack_bot = SlackBot()
-    # 채용 공고 존재
-    if file_path != "":
-        # GCP BigQudry에 업로드
-        insert_position(file_path)
 
-        # Slack에 csv 파일 전송
-        slack_bot.send_file(file_path)
+    try:
+        # day(today: 오늘만, all: 전부)
+        # save(true: csv 저장 O, false: csv 저장 X)
+        # saramin = Saramin(day="all", save=True)
+        saramin = Saramin(day="today", save=False)
+        saramin.crawling()
+        file_path = saramin.save_all_data_to_csv()
 
-        os.remove(file_path)
-    # 채용 공고 없음 -> "공고 없음" 메시지 전송
-    else:
-        slack_bot.send_message("오늘은 등록된 공고가 없습니다!")
+    except:
+        slack_bot.send_message("크롤링 중 에러가 발생했습니다!")
 
-    os.rmdir("result/")
+    try:
+        # 채용 공고 존재
+        if file_path != "":
+            # GCP BigQudry에 업로드
+            insert_position(file_path)
+
+            # Slack에 csv 파일 전송
+            slack_bot.send_file(file_path)
+
+            os.remove(file_path)
+        # 채용 공고 없음 -> "공고 없음" 메시지 전송
+        else:
+            slack_bot.send_message("오늘은 등록된 공고가 없습니다!")
+
+        os.rmdir("result/")
+    
+    except:
+        slack_bot.send_message("크롤링 이후 에러가 발생했습니다!\n")
